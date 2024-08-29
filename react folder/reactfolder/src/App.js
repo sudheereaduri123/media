be6d32b34c6abef7055507e8d84f980ac1e38745
sudheereaduri123 <eadurisudheer@gmail.com>
2024-08-29 12:26:57 +0530

    Add files via upload

---
+++ react folder/reactfolder/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='mainHeading'>Learning JSXML Rules</h1>
+     <img src="https://www.pixelstalk.net/wp-content/uploads/2016/07/Wallpapers-pexels-photo.jpg"></img>
+     <form>
+      <div>
+        <label htmlfor='emailinput'>Email</label>
+        <input id='emailInput'/>
+        <label htmlfor='password'>password</label>
+        <input id='password'/>
+      </div>
+     </form>
+
+
+    </div>
+  );
+}
+
+export default App;
